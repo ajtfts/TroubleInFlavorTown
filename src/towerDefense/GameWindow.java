cce17ea948af17043e1a6c016b3691ab432f84da
Aidan Taylor <aidantwothousand@gmail.com>
2018-10-01 20:20:12 -0600

    Mouse listener, move map view
    Can now drag around the view of the map with the mouse.

--- src/towerDefense/GameWindow.java
+++ src/towerDefense/GameWindow.java
@@ -1,7 +1,10 @@
 package towerDefense;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -22,7 +25,11 @@ public class GameWindow {
 	private JFrame frame;
 	private JPanel panel;
 	
+	private MouseListener mListener;
+	private boolean mouseState = false;
+	
 	private ArrayList<GameObject> renderList = new ArrayList<GameObject>();
+	private int xOffset = 0, yOffset = 0;
 	
 	private int mapWidth, mapHeight;
 	private String tileInfo = "";
@@ -46,8 +53,6 @@ public class GameWindow {
 				num++;
 			}
 			sc.close();
-			panel.setPreferredSize(new Dimension(mapWidth*TILE_SIZE, mapHeight*TILE_SIZE));
-			frame.pack();
 		} catch (FileNotFoundException e) {
 			System.out.println("Failed to find map file.");
 			e.printStackTrace();
@@ -57,7 +62,7 @@ public class GameWindow {
 	private void drawMap(Graphics g) {
 		for (int i = 0; i < mapHeight; i++) {
 			for (int j = 0; j < mapWidth; j++) {
-				g.drawImage(resArray[tileDict.get(tileInfo.charAt(i*mapWidth+j))], j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+				g.drawImage(resArray[tileDict.get(tileInfo.charAt(i*mapWidth+j))], j*TILE_SIZE+xOffset, i*TILE_SIZE+yOffset, TILE_SIZE, TILE_SIZE, null);
 			}
 		}
 	}
@@ -81,17 +86,33 @@ public class GameWindow {
 				
 				drawMap(g);
 				
+				// draw all GameObjects inside renderList
 				for (int i = 0; i < renderList.size(); i++) {
 					GameObject cur = renderList.get(i);
 					float[] pos = cur.getPos();
 					int[] dims = cur.getImgDims();
-					g.drawImage(resArray[cur.getImageID()], (int) pos[0], (int) pos[1], dims[0], dims[1], null);
+					g.drawImage(resArray[cur.getImageID()], (int) pos[0]+xOffset, (int) pos[1]+yOffset, dims[0], dims[1], null);
 				}
 			}
 		};
-		panel.setPreferredSize(new Dimension(200, 200));
+		
+		panel.setPreferredSize(new Dimension(1200, 675));
+		panel.setBackground(Color.DARK_GRAY);
 		frame.add(panel);
 		frame.pack();
+		
+		// mouse events
+		
+		mListener = new MouseListener() {
+			public void mouseClicked(MouseEvent e) {
+			}
+			public void mousePressed(MouseEvent e) {mouseState = true;}
+			public void mouseReleased(MouseEvent e) {mouseState = false;}
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+		};
+		
+		panel.addMouseListener(mListener);
 		
 		// setup a string to integer dictionary to determine what texture should be used for a particular character in the map file
 		tileDict.put('g', 1);
@@ -104,5 +125,22 @@ public class GameWindow {
 	
 	public ArrayList<GameObject> getRenderList() {
 		return this.renderList;
+	}
+	
+	public boolean getMouseState() {
+		return this.mouseState;
+	}
+	
+	public int getXOffset() {
+		return this.xOffset;
+	}
+	
+	public int getYOffset() {
+		return this.yOffset;
+	}
+	
+	public void setOffset(int x, int y) {
+		this.xOffset = x;
+		this.yOffset = y;
 	}
 }
--- src/towerDefense/RunGame.java
+++ src/towerDefense/RunGame.java
@@ -1,11 +1,13 @@
 package towerDefense;
 
+import java.awt.MouseInfo;
+import java.awt.Point;
 import java.util.ArrayList;
 
 public class RunGame {
 
-	static private GameWindow window;
-	static private ArrayList<GameObject> r;
+	private static GameWindow window;
+	private static ArrayList<GameObject> r;
 	
 	private static final int TARGET_FPS = 60;
 	private static final long OPTIMAL_TIME = 1000000000 / TARGET_FPS; // convert target frames-per-second to target time between frames in nanoseconds
@@ -16,15 +18,12 @@ public class RunGame {
 	public static void main(String[] args) {
 		window = new GameWindow();
 		r = window.getRenderList();
+		
 		// setup the initial state of the game
 		window.loadMap("maptest.txt");
 		
 		t1 = new Tower(0.0f, 0.0f, 32, 32, r);
 		t2 = new Tower(0.0f, 100.0f, 32, 32, r);
-		
-		// redraw panel
-		window.getMainPanel().revalidate();
-		window.getMainPanel().repaint();
 		
 		gameLoop();
 	}
@@ -36,16 +35,41 @@ public class RunGame {
 	}
 	
 	private static void gameLoop() {
+		
+		boolean first = true;
+		
 		long lastFrame = System.nanoTime();
 		
+		int difX = 0, difY = 0, mapX = 0, mapY = 0, initXOffset = 0, initYOffset = 0;
 		
 		while (running) {
 			long now = System.nanoTime();
 			long deltaT = now - lastFrame; 
+			
 			lastFrame = now;
 			
 			//update game state
 			updateLogic(deltaT);
+			
+			
+			// change map view by dragging mouse
+			if (window.getMouseState()) {
+				if (first) {
+					Point mPos = MouseInfo.getPointerInfo().getLocation();
+					mapX = (int) mPos.getX();
+					mapY = (int) mPos.getY();
+					initXOffset = window.getXOffset();
+					initYOffset = window.getYOffset();
+					first = false;
+				} else {
+					Point mPos = MouseInfo.getPointerInfo().getLocation();
+					difX = (int) mPos.getX() - mapX;
+					difY = (int) mPos.getY() - mapY;
+					window.setOffset(initXOffset+difX, initYOffset+difY);
+				}
+			} else {
+				first = true;
+			}
 			
 			// redraw
 			window.getMainPanel().repaint();
